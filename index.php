337fe705b16172205b9b5ae31b380e185d9364ae
DShivaram01 <117584247+DShivaram01@users.noreply.github.com>
2026-02-11 12:17:15 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  session_start();
+  require "db_connection.php";
+  
+  if(isset($_SESSION['user_id']))
+    {
+      header("Location: dashboard.php");
+      exit();
+    }
+
+    else{
+        header("Location: login.php");
+        exit();
+    }
+
+?>
